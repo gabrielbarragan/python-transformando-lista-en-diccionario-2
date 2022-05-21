a888737489a493e235431e00574c130447dcb975
gabrielbarragan <gabrielbarragan07@gmail.com>
2022-05-21 14:57:14 -0500

    tranformando listas en diccts 2 terminado

--- get-all-keys/get_all_keys.py
+++ get-all-keys/get_all_keys.py
@@ -1,20 +1,17 @@
-"""code goes here"""
 
-
-"""function to get unique values"""
-
+def get_unique_values(array):
+    """function to get unique values"""
     # initialize a null list
-    
-     
+    print (array)
+    null_list = []
     # traverse for all elements
+    unique_list = [null_list.append(field) for field in array if field not in null_list] 
     
-        # check if exists in unique_list or not
-        
-
     #returns unique_list
-     
+    return null_list
 
-
-
-"""function to get all keys"""
-
+def get_all_keys(*args):
+    """function to get all keys"""
+    my_list = [key for person in args for key in person.keys()]
+    my_list = get_unique_values(my_list)
+    return my_list
